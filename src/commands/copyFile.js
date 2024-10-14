17ad6a4cee8c0d9da90d2967ddeae669a1ec1bf1
Anar Hasanov <anar.hasanov79@gmail.com>
2024-10-14 14:14:53 +0400

    fix: copyFile

--- src/commands/copyFile.js
+++ src/commands/copyFile.js
@@ -1,12 +1,15 @@
 import { createReadStream, createWriteStream } from "fs";
 import stream from "stream/promises";
 import path from "path";
+import { access } from "fs/promises";
 
 export const copyFile = async (args) => {
   try {
     const [filePath, newDirectory] = args;
     const { base } = path.parse(filePath);
     const newFilePath = path.join(newDirectory, base);
+
+    await access(filePath);
 
     const readableStream = createReadStream(filePath);
     const writableStream = createWriteStream(newFilePath);
