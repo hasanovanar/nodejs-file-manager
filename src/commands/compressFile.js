9b4ba3815c6427575568959508941fd7ce0072de
Anar Hasanov <anar.hasanov79@gmail.com>
2024-10-14 14:42:46 +0400

    fix:handle error

--- src/commands/compressFile.js
+++ src/commands/compressFile.js
@@ -1,12 +1,15 @@
 import { createReadStream, createWriteStream } from "fs";
 import stream from "stream/promises";
 import zlib from "zlib";
+import { access } from "fs/promises";
 
 export async function compressFile(args) {
   try {
     const [filePath, destinationPath] = args;
 
     const destinationCompressed = `${destinationPath}.br`;
+
+    await access(filePath);
 
     const readableStream = createReadStream(filePath);
     const writableStream = createWriteStream(destinationCompressed);
--- src/commands/decompressFile.js
+++ src/commands/decompressFile.js
@@ -1,16 +1,13 @@
 import { createReadStream, createWriteStream } from "fs";
 import stream from "stream/promises";
-import path from "path";
 import zlib from "zlib";
+import { access } from "fs/promises";
 
 export async function decompressFile(args) {
   try {
     const [filePath, destinationPath] = args;
-    const { ext } = path.parse(filePath);
 
-    if (ext !== ".br") {
-      throw new Error("Invalid input");
-    }
+    await access(filePath);
 
     const readableStream = createReadStream(filePath);
     const writableStream = createWriteStream(destinationPath);
